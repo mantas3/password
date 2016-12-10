0b9fe9a914ede5bb7c5fea60a76a9fb3119af3d0
manty <mantas.armonavicius@gmail.com>
2016-12-11 00:56:21 +0200

    .

--- index.php
+++ index.php
@@ -1,6 +1,11 @@
 <?php
-function random_str($length = 32, $keyspace = '!#$%&()*+,-./0123456789:;=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\]^_`abcdefghijklmnopqrstuvwxyz{|')
+function random_str($length = 32, $complex = true)
 {
+    if($complex) {
+        $keyspace = '!#$%&()*+,-./0123456789:;=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\]^_`abcdefghijklmnopqrstuvwxyz{|';
+    } else {
+        $keyspace = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz';
+    }
     $str = '';
     $max = mb_strlen($keyspace, '8bit') - 1;
     for ($i = 0; $i < $length; ++$i) {
@@ -12,21 +17,51 @@ function random_str($length = 32, $keyspace = '!#$%&()*+,-./0123456789:;=>?@ABCD
 <html>
 <head>
     <title>Password generator</title>
+    <style>
+        button {
+            width: 100px;
+            height: 50px;
+        }
+        input {
+            width: 500px;
+            height: 50px;
+            font-size: large;
+            text-align: center;
+            margin:  0 25px 25px 25px;
+        }
+    </style>
 </head>
 <body>
+<h2 align="center">Simple password (16 characters, letters and numbers)</h2>
 <div align="center">
-    <input id="password" style="width: 500px; height: 50px; font-size: large; text-align: center; margin: 25px" value="<?php echo random_str();?>">
+    <input id="password-simple" value="<?php echo random_str(16, false);?>">
 </div>
 <div align="center">
-    <button id="generate-button" style="width: 100px; height: 50px">Generate</button>
-    <button id="copy-button" style="width: 100px; height: 50px" data-clipboard-target="#password">Copy</button>
+    <button onclick="reloadPage()">Generate</button>
+    <button onclick="copyPassword('password-simple')">Copy</button>
 </div>
+
+
+<h2 align="center">Complex password (32 characters, letters, numbers, punctuation)</h2>
+<div align="center">
+    <input id="password" value="<?php echo random_str();?>">
+</div>
+<div align="center">
+    <button onclick="reloadPage()">Generate</button>
+    <button onclick="copyPassword()">Copy</button>
+</div>
+
 <script>
-    document.querySelector("#generate-button").onclick = function() {
+    function reloadPage() {
         document.location.reload();
-    };
-    document.querySelector("#copy-button").onclick = function() {
-        document.querySelector("#password").select();
+    }
+
+    function copyPassword(button = "password") {
+        if(button == "password") {
+            document.querySelector("#password").select();
+        } else {
+            document.querySelector("#password-simple").select();
+        }
         document.execCommand('copy');
-    };
+    }
 </script>
